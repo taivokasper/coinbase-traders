e88ba2db8f3588005848cfab1aef55ca5a8bc9e5
Taivo Käsper <taivokasper@Taivos-MacBook-Pro.local>
2015-01-11 23:54:46 +0200

    Changed collection name because stats is predefined in mongodb

--- src/main/java/com/coinbasetraders/model/Stats.java
+++ src/main/java/com/coinbasetraders/model/Stats.java
@@ -2,10 +2,12 @@ package com.coinbasetraders.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Document(collection = "priceStats")
 @Data
 @AllArgsConstructor
 public class Stats {
